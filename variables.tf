44701f8ebbdc9e7936a857e0a7cf0e5323975d00
Victor Colmenares <60989805+vicolmenares@users.noreply.github.com>
2024-02-21 18:44:54 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,11 @@ variable "region" {
   description = "The primary AWS region where all the resources will be created. See https://docs.aws.amazon.com/general/latest/gr/rande.html"
 }
 
-
+variable "tags" {
+  description = "AWS Tags to add to all resources created (where possible); see https://aws.amazon.com/answers/account-management/aws-tagging-strategies/"
+  type        = "map"
+  default     = {}
+}
 variable "site_domain" {
   description = "The primary domain name of the website"
 }
